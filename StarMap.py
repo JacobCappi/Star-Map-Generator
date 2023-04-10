fc219432d54c4345c9d9404659c8aae855e0e073
Wew0011 <wew0011@uah.edu>
2023-04-09 21:38:40 -0500

    bruh

--- StarMap.py
+++ StarMap.py
@@ -2,14 +2,20 @@ import tkinter as tk
 from Star import Star
 from Planets import Planet
 from MathEquations import MathEquations
-from Contellations import Constellations
+from Constellations import Constellations
 from MessierObjects import MessierObjects
 from PIL import Image, ImageTk
 from datetime import datetime
 from datetime import timezone
 import csv
 import time
-
+from astropy.coordinates import SkyCoord, AltAz, EarthLocation
+import numpy as np
+from astropy import units as u
+from astropy.coordinates import SkyCoord, AltAz
+from astropy.time import Time
+import matplotlib.pyplot as plt
+import math
 
 class StarMap:
     # TO map (UI) : az N:0, E:90, S:180, W:270
@@ -40,6 +46,8 @@ class StarMap:
     _isNorth = True
     _isEast = False
 
+    _location = 0
+    _elv = 0
     
     # TODO: change this to input
     _time = datetime.now(timezone.utc)
@@ -140,32 +148,93 @@ class StarMap:
         image.save(filename, 'JPEG')
         return image
 
-    def show_on_screen(self, star_map_image):
+    def show_on_screen(self):
         window = tk.Tk()
         window.title("Star Map")
+        # Create a canvas widget with a white background
+        canvas = tk.Canvas(window, width=1920, height=1080, bg="black")
+        canvas.pack()
 
-        # Open the star map image
-        image = Image.open(star_map_image)
-        image = image.resize((800, 600))
-        image = ImageTk.PhotoImage(image)
+        for star in self._stars:
+            x = math.cos(star[3]) * math.sin(star[2])
+            y = math.cos(star[3]) * math.cos(star[2])
+            print(x, y)
+            x *= 600
+            y *= 600
 
-        # Display the image in a scrollable window
-        canvas = tk.Canvas(window, width=800, height=600)
-        canvas.pack(side="left", fill="both", expand=True)
-        canvas.create_image(0, 0, anchor="nw", image=image)
-        canvas.config(scrollregion=canvas.bbox(tk.ALL))
+            x += 810
+            y += 540
 
-        # Add scrollbars to the window
-        scrollbar = tk.Scrollbar(window, orient="vertical", command=canvas.yview)
-        scrollbar.pack(side="right", fill="y")
-        canvas.configure(yscrollcommand=scrollbar.set)
-
+    # After init, list of all stars [starid, starname, az, alt]
+            canvas.create_oval(x, y, x+1, y+1, fill="white")
         # Show the window
-        window.mainloop()
+        window.mainloop() 
+        
+    def drawstars(self):
+
+        starmap = tk.Canvas(width=1920, height=1080)
+        starmap.config(scrollregion=(0, 0, 1920, 1080)) # make it scrollable
+
+        scrollbar = tk.Scrollbar(starmap, orient=tk.VERTICAL)  # Add a scrollbar to the canvas
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        scrollbar.config(command=starmap.yview)
+        scrollbar2 = tk.Scrollbar(starmap, orient=tk.HORIZONTAL)  # Add a scrollbar to the canvas
+        scrollbar2.pack(side=tk.BOTTOM, fill=tk.X)
+        scrollbar2.config(command=starmap.yview)
+
+        starmap.config(yscrollcommand=scrollbar.set)
+        starmap.config(xscrollcommand=scrollbar2.set)
+
+        # Add an image to the canvas
+        image = tk.PhotoImage(file="image.png")
+        starmap.create_image(0, 0, anchor=tk.NW, image=image)
+
+        # Display the canvas
+        starmap.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        
+        canvas_width = 1920
+        canvas_height = 1080
+
+        # define the range of x and y coordinates
+        x_min = -1
+        x_max = 1
+        y_min = -1
+        y_max = 1
+
+        # calculate the scaling factors
+        x_scale = canvas_width / (x_max - x_min)
+        y_scale = canvas_height / (y_max - y_min)
+
+        
+        
+        
+        # After init, list of all stars [starid, starname, az, alt]
+        self._location = EarthLocation.from_geodetic(self._long, self._lat, self._elv*u.m)
+        print(self._location)
+        for star in self._stars:
+            print(star)
+            #azi, alti = self._equations.ConvertRAandDecToAziAndAlt(star.ra, star.dec)
+             
+            coord = SkyCoord(unit = "deg", alt=star[3], az=star[2], frame=AltAz(obstime=self._time, location=self._location))
+            x = coord.cartesian.x.value
+            y = coord.cartesian.y.value
+            print (x,y)
+            x_scaled = (x - x_min) * x_scale
+            y_scaled = (y - y_min) * y_scale
+            # plot the star on the canvas
+            starmap.create_oval(y_scaled - 2, x_scaled - 2, y_scaled +  2, x_scaled + 2, fill="white")
+       
+        
+            
+        
+        # Show the window
+        starmap.mainloop()
 
 # Create a main function to run the program
 def main():
     star_map = StarMap()
+    star_map.load_star_catalog()
+    star_map.show_on_screen()
 
     
 
